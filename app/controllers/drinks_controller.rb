3a5ca13f1416fc08534e2327b3f548ddccf6916e
Natan Kruger <natan.kruger@gmail.com>
2019-06-18 22:09:13 -0300

    renwal

--- app/controllers/drinks_controller.rb
+++
@@ -1,5 +0,0 @@
-class DrinksController < ApplicationController
-  def index
-    @drinks = Drink.all.order(:created_at)
-  end
-end
--- vendor/bundle/ruby/2.4.0/gems/rails-assets-bootstrap-4.0.0.alpha.6/lib/rails-assets-bootstrap/version.rb
+++
@@ -1,3 +0,0 @@
-module RailsAssetsBootstrap
-  VERSION = "4.0.0.alpha.6"
-end
