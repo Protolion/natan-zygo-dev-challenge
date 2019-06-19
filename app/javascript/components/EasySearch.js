92e96b61df9bf1a6e1f9263ff9715103df610377
Natan Kruger <natan.kruger@gmail.com>
2019-06-19 13:49:30 -0300

    New stuff

---
+++ app/javascript/components/EasySearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class EasySearch extends React.Component {
+  render () {
+    return (
+      <div className="simple-form">
+          
+                <div className="simple-form__input">
+                    <input className="form-control form-control-lg" 
+                          type="text" value={this.props.simpleSearch} 
+                          onChange={this.props.updateSimpleSearch.bind(this)} 
+                          placeholder="Try a Martini.."/>
+                </div>
+          </div>
+    );
+  }
+}
+
+export default EasySearch;
